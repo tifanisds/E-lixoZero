5573fbc33df262587955fa43b187de26cc511d34
tifani <tifanisds@gmail.com>
2025-05-11 23:37:46 -0300

    Feat: Adiciona a página de locais de coleta

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
             <img src="./assets/img/logo.png" alt="">
           </div>
           <div class="navbar-nav">
-            <router-link class="nav-link" to="/locationsPage">Pontos de coleta</router-link>
+            <router-link class="nav-link" to="/">Pontos de coleta</router-link>
             <router-link class="nav-link" to="/aboutEletronicWastePage">Conscientize-se</router-link>
             <router-link class="nav-link" to="/aboutUsPage">Sobre o projeto</router-link>
           </div>
